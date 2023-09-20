59745cea5776c7f31713c8a22f87878a7e03af54
farrasdoko <farras.abiyyu.doko@gmail.com>
2023-09-20 16:05:39 +0700

    Project Initialization

---
+++ MobileAppPromoFarrasTests/MobileAppPromoFarrasTests.swift
@@ -0,0 +1,12 @@
+//
+//  MobileAppPromoFarrasTests.swift
+//  MobileAppPromoFarrasTests
+//
+//  Created by Farras on 20/09/23.
+//
+
+import XCTest
+@testable import MobileAppPromoFarras
+
+final class MobileAppPromoFarrasTests: XCTestCase {
+}
